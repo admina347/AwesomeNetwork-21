4288d61001dea2e79a919f2e12fc34abb1b4c3b8
admina <it-mactep@mail.ru>
2023-04-03 10:41:26 +0500

    AwesomeNetwork.Web init

---
+++ AwesomeNetwork.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AwesomeNetwork.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
